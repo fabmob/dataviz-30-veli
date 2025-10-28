03143fc487ab42f40e5de978e2588a1c84bc2a42
TTalex <alex.bourreau@free.fr>
2025-10-28 19:51:03 +0100

    Improve Disk display and add content

--- frontend/components/Disque.tsx
+++ frontend/components/Disque.tsx
@@ -47,7 +47,19 @@ const SliceLabelPlugin = {
 
         ctx.save()
         ctx.fillStyle = "#333"
-        ctx.font = "bold 8px sans-serif"
+        let fontSize
+        switch (datasetIndex) {
+            case 0:
+                fontSize = 8
+                break
+            case 1:
+                fontSize = 12
+                break
+            case 2:
+                fontSize = 16
+                break
+        }
+        ctx.font = `bold ${fontSize}px sans-serif`
         ctx.textAlign = "center"
         ctx.textBaseline = "middle"
         const lines = getLines(ctx, label, 40)
@@ -63,42 +75,42 @@ const SliceLabelPlugin = {
 }
 
 const outerCatLabels = [
-  "1.1.1 Qualité de la formation à la prise en main",
-  "1.1.2 Qualité de la notice d’emploi",
-  "1.1.3 Facilité d’utilisation du véhicule ",
-  "1.1.4 Taux d'implication des testeurs dans la méthode de suivi de l'expérimentation",
-  "1.2.1 Représentativité du territoire",
-  "1.2.2 Profil des usagers en accord aux cibles de l'expérimentation (identifiées au départ)",
-  "1.2.3 Utilisation effective du véli pendant le prêt à l'usager",
-  "1.2.4 Infractions, accidents",
-  "1.2.5 Nombre d’expérimentateurs en accord au nombre visé au départ",
-  "1.2.6 Km parcourus / commune / expérimentateurs",
-  "1.3.1 Utilité perçue",
-  "1.3.2 Volonté du béneficiaire de pérenniser l'usage ",
-  "2.1.1 Sentiment de sécurité au bord du véli",
-  "2.1.2 Autonomie et consommation électrique",
-  "2.1.3 Adaptabilité au dénivelé",
-  "2.1.4 Stabilité * (faire évouler)",
-  "2.1.5 Adaptabilité aux intempéries*",
-  "2.1.6 Flexibilité organisationnelle des véhicules",
-  "2.1.7 Maintenance corrective",
-  "2.2.1 Sentiment de securité au bord du véli en fonction des infrastructures routières",
-  "2.2.2 Qualité de l’espace public adapté à la ciculation de vélis",
-  "2.2.3 Adaptabilité des stationnements aux VELI",
-  "3.1.1 Effort de lancement ",
-  "3.2.1 Maintenance préventive ",
-  "3.3.1 Moyens humains dediés à la mise en place et suivi du projet ",
-  "3.3.2 Implication effective des parties prenantes associées dans le suivi du projet",
-  "4.1.1 Plan communication tout au long du dispositif d'expérimentation",
-  "4.1.2 Visibilité des enjeux autour des vélis ",
-  "4.2.1 Changement de comportement vis à vis de la voiture",
-  "4.2.2 Sensibilisation au partage de la voirie ",
-  "4.2.3 Meilleure connaissance des VELI",
-  "4.2.4 Sentiment du bien-être dans l'utilisation du véli",
-  "4.3.1 Seuil d’acceptabilité d’achat  ou location/ service rendu",
-  "4.3.2 Gain sur coût carburant",
-  "4.4.1 Bilan CO2 des trajets / CO2 évité",
-  "4.4.2 Sensibilisation aux enjeux environnementaux",
+  "Qualité de la formation à la prise en main", // 1.1.1 
+  "Qualité de la notice d’emploi", // 1.1.2 
+  "Facilité d’utilisation du véhicule ", // 1.1.3 
+  "Taux d'implication des testeurs dans la méthode de suivi de l'expérimentation", // 1.1.4 
+  "Représentativité du territoire", // 1.2.1 
+  "Profil des usagers en accord aux cibles de l'expérimentation (identifiées au départ)", // 1.2.2 
+  "Utilisation effective du véli pendant le prêt à l'usager", // 1.2.3 
+  "Infractions, accidents", // 1.2.4 
+  "Nombre d’expérimentateurs en accord au nombre visé au départ", // 1.2.5 
+  "Km parcourus / commune / expérimentateurs", // 1.2.6 
+  "Utilité perçue", // 1.3.1 
+  "Volonté du béneficiaire de pérenniser l'usage ", // 1.3.2 
+  "Sentiment de sécurité au bord du véli", // 2.1.1 
+  "Autonomie et consommation électrique", // 2.1.2 
+  "Adaptabilité au dénivelé", // 2.1.3 
+  "Stabilité * (faire évouler)", // 2.1.4 
+  "Adaptabilité aux intempéries*", // 2.1.5 
+  "Flexibilité organisationnelle des véhicules", // 2.1.6 
+  "Maintenance corrective", // 2.1.7 
+  "Sentiment de securité au bord du véli en fonction des infrastructures routières", // 2.2.1 
+  "Qualité de l’espace public adapté à la ciculation de vélis", // 2.2.2 
+  "Adaptabilité des stationnements aux VELI", // 2.2.3 
+  "Effort de lancement ", // 3.1.1 
+  "Maintenance préventive ", // 3.2.1 
+  "Moyens humains dediés à la mise en place et suivi du projet ", // 3.3.1 
+  "Implication effective des parties prenantes associées dans le suivi du projet", // 3.3.2 
+  "Plan communication tout au long du dispositif d'expérimentation", // 4.1.1 
+  "Visibilité des enjeux autour des vélis ", // 4.1.2 
+  "Changement de comportement vis à vis de la voiture", // 4.2.1 
+  "Sensibilisation au partage de la voirie ", // 4.2.2 
+  "Meilleure connaissance des VELI", // 4.2.3 
+  "Sentiment du bien-être dans l'utilisation du véli", // 4.2.4 
+  "Seuil d’acceptabilité d’achat  ou location/ service rendu", // 4.3.1 
+  "Gain sur coût carburant", // 4.3.2 
+  "Bilan CO2 des trajets / CO2 évité", // 4.4.1 
+  "Sensibilisation aux enjeux environnementaux", // 4.4.2 
 ]
 const outerValues = [
   2.083333333,
@@ -140,44 +152,65 @@ const outerValues = [
 ]
 
 const colors = {
-  "Fort": "#0dadae",
-  "Moyen": "#7fdbdb",
-  "Faible": "#cbf2f2",
-  "fort/moyen/faible": "white",
-  "N/A": "white",
+  "fort": "#0dadae",
+  "moyen": "#7fdbdb",
+  "faible": "#cbf2f2"
+}
+const getColor = (label) => {
+  return colors[label] || "white"
 }
 
-const getMostFrequentLabel = (i, j, dlabels) => {
-  const array = dlabels.slice(i, j)
+// const getMostFrequentLabel = (i, j, dlabels) => {
+//   const array = dlabels.slice(i, j)
+//   if (array.length === 0) return "N/A"
+//   const mostFrequent = Array.from(new Set(array)).reduce((prev, curr) =>
+//     array.filter(el => el === curr).length > array.filter(el => el === prev).length ? curr : prev
+//   ) as string
+//   return mostFrequent
+// }
+
+const getAverageLabel = (i, j, dlabels) => {
+  const array = dlabels.slice(i, j).filter(e => e !== "N/A").map(e => {
+    switch (e) {
+      case "faible":
+        return 0
+      case "moyen":
+        return 1
+      case "fort":
+        return 2
+    }
+  })
   if (array.length === 0) return "N/A"
-  const mostFrequent = Array.from(new Set(array)).reduce((prev, curr) =>
-    array.filter(el => el === curr).length > array.filter(el => el === prev).length ? curr : prev
-  ) as string
-  return mostFrequent
+  const average = array.reduce((prev, curr) => prev + curr, 0) / array.length
+  if (average < 0.66) return "faible"
+  if (average < 1.33) return "moyen"
+  return "fort"
 }
-
 
 const Disque = ({evaluations}: {evaluations: string[]}) => {
-  const dataLabels = evaluations
+  const dataLabels = evaluations.map(e => {
+    const label = e.toLowerCase()
+    return colors[label] ? label : "N/A"
+  })
   const criteresDataLabels = [
-    getMostFrequentLabel(0, 4, dataLabels),
-    getMostFrequentLabel(4, 10, dataLabels),
-    getMostFrequentLabel(10, 12, dataLabels),
-    getMostFrequentLabel(12, 19, dataLabels),
-    getMostFrequentLabel(19, 22, dataLabels),
-    getMostFrequentLabel(22, 23, dataLabels),
-    getMostFrequentLabel(23, 24, dataLabels),
-    getMostFrequentLabel(24, 26, dataLabels),
-    getMostFrequentLabel(26, 28, dataLabels),
-    getMostFrequentLabel(28, 32, dataLabels),
-    getMostFrequentLabel(32, 34, dataLabels),
-    getMostFrequentLabel(34, 36, dataLabels),
+    getAverageLabel(0, 4, dataLabels),
+    getAverageLabel(4, 10, dataLabels),
+    getAverageLabel(10, 12, dataLabels),
+    getAverageLabel(12, 19, dataLabels),
+    getAverageLabel(19, 22, dataLabels),
+    getAverageLabel(22, 23, dataLabels),
+    getAverageLabel(23, 24, dataLabels),
+    getAverageLabel(24, 26, dataLabels),
+    getAverageLabel(26, 28, dataLabels),
+    getAverageLabel(28, 32, dataLabels),
+    getAverageLabel(32, 34, dataLabels),
+    getAverageLabel(34, 36, dataLabels),
   ]
   const axesDataLabels = [
-    getMostFrequentLabel(0, 12, dataLabels),
-    getMostFrequentLabel(12, 22, dataLabels),
-    getMostFrequentLabel(22, 26, dataLabels),
-    getMostFrequentLabel(26, 36, dataLabels),
+    getAverageLabel(0, 12, dataLabels),
+    getAverageLabel(12, 22, dataLabels),
+    getAverageLabel(22, 26, dataLabels),
+    getAverageLabel(26, 36, dataLabels),
   ]
   const data = {
     labels: [],
@@ -188,17 +221,17 @@ const Disque = ({evaluations}: {evaluations: string[]}) => {
         data: outerValues,
         catlabels: outerCatLabels,
         datalabels: dataLabels,
-        backgroundColor: dataLabels.map(d => colors[d]),
-        borderColor: "#0dacad"
+        backgroundColor: dataLabels.map(getColor),
+        borderColor: "#086b6bff"
       },
       {
         // Middle ring
         label: "Critères",
         data: [8.333333333, 8.333333333, 8.333333333, 12.5, 12.5, 8.333333333, 8.333333333, 8.333333333, 6.25, 6.25, 6.25, 6.25],
-        catlabels: ["1.1 Compréhension","1.2 Expérience","1.3 Valeur","2.1 Véhicule","2.2 Voirie","3.1 Lancement","3.2 Maintenance","3.3 Mise en place et suivi","4.1 Communication","4.2 Social","4.3 Économique","4. 4 Environnemental"],
+        catlabels: ["Compréhension","Expérience","Valeur","Véhicule","Voirie","Lancement","Maintenance","Mise en place et suivi","Communication","Social","Économique","Environnemental"],
         datalabels: criteresDataLabels,
-        backgroundColor: criteresDataLabels.map(d => colors[d]),
-        borderColor: "#0dacad"
+        backgroundColor: criteresDataLabels.map(getColor),
+        borderColor: "#086b6bff"
       },
       {
         // Inner ring
@@ -206,8 +239,8 @@ const Disque = ({evaluations}: {evaluations: string[]}) => {
         data: [25, 25, 25, 25],
         catlabels: ["Usages", "Robustesse", "Moyens", "Impact"],
         datalabels: axesDataLabels,
-        backgroundColor: axesDataLabels.map(d => colors[d]),
-        borderColor: "#0dacad"
+        backgroundColor: axesDataLabels.map(getColor),
+        borderColor: "#086b6bff"
       }
     ]
   }
--- frontend/pages/Disques.tsx
+++ frontend/pages/Disques.tsx
@@ -1,44 +1,116 @@
-import React, { useState } from "react"
+import React, { use, useState, useEffect } from "react"
 import Disque from "../components/Disque"
 
+const evaluationsData = {
+    "Commune du Teil": {
+        "vehicles": {
+            "Urbaner": ["Moyen","Fort","Fort","Moyen","Moyen","Moyen","Moyen","Fort","","fort/moyen/faible","Fort","Moyen","Fort","Moyen","Faible","Moyen","Moyen","Moyen","Moyen","Moyen","Fort","Fort","Fort","Moyen","Moyen","Fort","Fort","Moyen","Moyen","Faible","Fort","Fort","Moyen","Faible","Faible","Fort"],
+            "BIRO": ["","","","Moyen","Moyen","Moyen","faible","","","","faible","","","","","fort","","","","Moyen","Fort","Fort","Fort","","Moyen","Fort","Fort","Moyen","Moyen","Faible","Fort","Faible","Faible","","","Fort"],
+            "WEEZ": ["","","","Moyen","Moyen","Moyen","Moyen","","","","","","","","","fort","","","","Moyen","Fort","Fort","Fort","","Moyen","Fort","Fort","Moyen","Moyen","Faible","Fort","Moyen","Faible","","","Fort"],
+            "Cyclospace": ["","","","Moyen","Moyen","Moyen","moyen","","","","Fort","Moyen","","","","moyen","","","","Moyen","Fort","Fort","Fort","","Moyen","Fort","Fort","Moyen","Moyen","Faible","Fort","Fort","Faible","","","Fort"]
+        },
+        "desc": `Territoire : Le Teil
+            Région : Auvergne-Rhône-Alpes
+            Zone : Rurale (La ville compte deux quartiers prioritaires, concentrant plus d’un quart des habitants)
+            Dénivelés : moyens
+            Periode d’expérimentation : de janvier 2024 au décembre 2025
+            Vélis testés : Biro, Weez, 2 Urbaner, Cyclospace
+            Cas d’usage testés : trajets proffesionels des salaries de deux entreprises à but d’emploi du territoire ; déplacement du qouotidien des habitants : courses, trajets domicile-travail, loisirs… ; livraison des répas : Déclic et des Claps (Entreprise à But d’Emploi) a réalisé des livraisons de repas en Urbaner ; prêt du cyclospace à une agence inmobilière pour les trajets proffesionels avec des clients.
+            Profil d’usagers : Salariés et habitants
+            Parties prenantes du projet : Commune et Associations Mobilité 07-26 et ActiviTeil
+            Fiche wiki pour savoir plus : https://wikixd.fabmob.io/wiki/Commune_Le_Teil
+        `
+    },
+    "Grand Pic Saint Loup": {
+        "vehicles": {
+            "BIRO": ["","","","moyen","moyen","moyen","moyen","","fort","","Fort","","","","","Moyen","","","","","","","Fort","","Fort","Moyen","Fort","moyen","moyen","fort","fort","faible","faible","","","Fort"],
+            "WEEZ": ["","","","moyen","moyen","moyen","fort","","fort","","Faible","","","","","Moyen","","","","","","","Fort","","Fort","Moyen","Fort","moyen","moyen","fort","fort","faible","moyen","","",""],
+            "Urbaner": ["","","","moyen","moyen","moyen","faible","","fort","","Moyen","","","","","Faible","","","","","","","Fort","","Fort","Moyen","Fort","moyen","moyen","fort","fort","moyen","Faible","","","fort"],
+        },
+        "desc": `Territoire : Communauté de Communes de Grand Pic Saint Loup
+            Région : Occitanie
+            Zone : Périurbaine (36 communes, 57 hectares, 50 892 (2022) habitants)
+            Dénivelés moyens : relief plus ou moins prononcé, 90% d’espaces naturels et agricoles
+            Periode d’expérimentation : octobre 2023 à décembre 2025
+            Vélis testés : Biro, Weez, Urbaner
+            Cas d’usage testés : Trajets du quotidien, déplacement des agents communaux, prévention incendie
+            Profil d’usagers : Actifs, retraités, agents communaux
+            Parties prenantes du projet : CC, Département de l’Herault, Association VELO VTT CLUB de ST-MATHIEU, Cerema
+            Fiche wiki pour savoir plus : : https://wikixd.fabmob.io/wiki/CC_Grand_Pic_Saint_Loup`
+    },
+    "CC Clunisois": {
+        "vehicles": {
+            "BIRO": ["","","","faible","Moyen","Fort","Moyen","","moyen","","faible","","","","","Moyen","","","","","","","Fort","","Moyen","Fort","moyen","moyen","Moyen","moyen","fort","Moyen","Faible","","","Fort",],
+            "WEEZ": ["","","","faible","Moyen","Fort","Moyen","","moyen","","faible","","","","","Fort","","","","","","","Fort","","Moyen","Fort","moyen","moyen","Moyen","moyen","fort","faible","faible","","","Fort"],
+            "Urbaner": ["","","","faible","Moyen","Fort","Faible","","moyen","","faible","","","","","moyen","","","","","","","Fort","","Moyen","Fort","moyen","moyen","Moyen","moyen","fort","Fort","faible","","","Fort"],
+            "Formidable": ["","","","faible","Moyen","Fort","Moyen","","moyen","","faible","","","","","moyen","","","","","","","Fort","","Moyen","Fort","moyen","moyen","Moyen","moyen","fort","fort","faible","","","Fort"],
+        },
+        "desc": `Territoire : Communauté de communes du Clunisois
+            Région : Bourgogne Franche Comté
+            Zone : rurale
+            Dénivelés : moyens
+            Vélis testés : Biro, Weez, Urbaner, Galian
+            Periode d’expérimentation : janvier 2024 - décembre 2025
+            Cas d’usage testés : déplacements professionnels des agents de la CC du Clunisois et trajets du quotidien : domicile – travail, courses, loisirs…
+            Profil d’usagers : Salariés, retraités, associations.
+            Parties prenantes du projet : Communauté de Communes, Cerema, Département de Saône et Loire
+            Fiche wiki pour savoir plus : https://wikixd.fabmob.io/wiki/Communauté_de_communes_du_Clunisois
+        `
+    },
+    "Avant Pays Savoyard": {
+        "vehicles": {
+            "Acticyle": ["","","","moyen","moyen","fort","moyen","","","","moyen","","","","","fort","","","","","","","fort","","moyen","moyen","fort","faible","","moyen","fort","fort","","","","faible"],
+        },
+        "desc": `Territoire : Avant Pays Savoyard
+            Région : Auvergne-Rhône-Alpes
+            Zone : rurale
+            Dénivelés : moyens
+            Periode d’expérimentation : janvier 2025 - décembre 2025
+            Vélis testés : 2 Acicycle
+            Cas d’usage testés : trajets du quotidien
+            Profil d’usagers : actifs et retaités (trajets domicile-travail à pas plus de 10km)
+            Parties prenantes du projet : SMAPS, Asso Vélo Solaire Pour Tous, ACT 4 Future, Mairie de St-Genix Les Villages, Agence écomobilité Savoie-Mont-Blanc
+            Fiche wiki pour savoir plus : https://wikixd.fabmob.io/wiki/Avant-pays_savoyard
+        `
+    }
+}
+const Select = ({options, selected, onChange}) => {
+    return (
+        <div className="select ml-2 mr-2">
+            <select value={selected} onChange={(e) => onChange(e.target.value)}>
+                {options.map((option, index) => <option key={index} value={option}>{option}</option>)}
+            </select>
+        </div>
+    )
+}
+
+const Desc = ({description}) => {
+    const splitDescription = description.split("\n")
+    return (
+        <table>
+            <tbody>
+                {splitDescription.map((line, index) => 
+                    <tr key={index}><td>{line.split(":")[0]}</td><td>{line.split(":").slice(1).join(":")}</td></tr>
+                )}
+            </tbody>
+        </table>
+    )
+}
 const Disques = () => {
-    const [evaluations, setEvaluations] = useState<string[]>([
-        "Moyen",
-        "Fort",
-        "Fort",
-        "Moyen",
-        "Moyen",
-        "Moyen",
-        "Moyen",
-        "Fort",
-        "N/A",
-        "Fort",
-        "Moyen",
-        "Fort",
-        "Moyen",
-        "Faible",
-        "Moyen",
-        "Moyen",
-        "Moyen",
-        "Moyen",
-        "Moyen",
-        "Fort",
-        "Fort",
-        "Fort",
-        "Moyen",
-        "Moyen",
-        "Fort",
-        "Fort",
-        "Moyen",
-        "N/A",
-        "Faible",
-        "Fort",
-        "Fort",
-        "Moyen",
-        "Faible",
-        "Faible",
-        "Fort"
-    ])
+    const [location, setLocation] = useState("Le Teil")
+    const [vehicule, setVehicule] = useState("Urbaner")
+    const [evaluations, setEvaluations] = useState([])
+
+    const availableLocations = Object.keys(evaluationsData)
+    const availableVehicules = Object.keys(evaluationsData[location].vehicles)
+    
+    useEffect(() => {
+        setVehicule(availableVehicules[0])
+    }, [location])
+
+    useEffect(() => {
+        setEvaluations(evaluationsData[location].vehicles[vehicule])
+    }, [vehicule])
     return (
         <div className="main">
             <section className="section">
@@ -47,8 +119,16 @@ const Disques = () => {
                         <h1 className="title">
                             Expérimentation 30 VELI, disque d'évaluation
                         </h1>
+                        <div style={{"lineHeight": "40px"}}>
+                            Territoire
+                            <Select options={availableLocations} selected={location} onChange={(value) => setLocation(value)}/>
+                            VELI 
+                            <Select options={availableVehicules} selected={vehicule} onChange={(value) => setVehicule(value)}/>
+                        </div>
+
                         {evaluations && evaluations.length && <Disque evaluations={evaluations}/>}
-                        <textarea className="textarea" value={evaluations.join("\n")} onChange={(e) => setEvaluations(e.target.value.split("\n").filter(e => e !== ""))} />
+                        {<Desc description={evaluationsData[location].desc}/>}
+                        {/* <textarea className="textarea" value={evaluations.join("\n")} onChange={(e) => setEvaluations(e.target.value.split("\n").filter(e => e !== ""))} /> */}
                     </div>
                 </div>
             </section>
